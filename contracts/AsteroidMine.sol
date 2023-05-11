942a217359216f1b42a64035bee6d7683b614271
BowTiedAnorak <bowtiedanorak@proton.me>
2023-05-11 21:08:29 +0100

    Make AsteroidMine ERC721Receivable & include rewardRate in Rewards Calculation

--- contracts/AsteroidMine.sol
+++ contracts/AsteroidMine.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "interfaces/IMintableERC20.sol";
 
 /**
@@ -12,7 +13,7 @@ import "interfaces/IMintableERC20.sol";
  * Staking spaceships will change the rate of rewards.
  * Based on: https://github.com/andreitoma8/Synthetix-ERC721-Staking/blob/master/contracts/ERC721Staking.sol
  */
-contract AsteroidMine is ReentrancyGuard {
+contract AsteroidMine is ReentrancyGuard, ERC721Holder {
 
     // Variables to store the contract addresses of the NFTs & Tokens the
     // Asteroid Mine will interact with.
@@ -23,15 +24,15 @@ contract AsteroidMine is ReentrancyGuard {
     // Variables to keep track of state of the contract.
     uint256 public totalRatsStaked;
     // 1 Reward Per Second per Rat Staked
-    uint256 rewardRate = 1;
+    uint256 public rewardRate = 1;
     // uint256 spaceshipBoostRate = 1;
     
     // Variables to keep track of assets staked.
     // Mappings from Address to Token & vice versa.
     mapping(uint256 => address) public stakedAssets;
-    mapping(address => uint256[]) public ratsStaked;
+    mapping(address => uint256[]) public ratsStakedIds;
     // Required to find an NFT ID when a staker has multiple NFTs staked.
-    // (ratsStaked is an array).
+    // (ratsStakedIds is an array).
     mapping(uint256 => uint256) public spaceRatIdToIndex;
     
     // Stores the rewards a user has accrued.
@@ -52,10 +53,9 @@ contract AsteroidMine is ReentrancyGuard {
         uint256 amount = spaceRatIds.length;
         for (uint256 i = 0; i < amount; i += 1) {
             SPACE_RAT_NFT.safeTransferFrom(msg.sender, address(this), spaceRatIds[i]);
-
             stakedAssets[spaceRatIds[i]] = msg.sender;
-            ratsStaked[msg.sender].push(spaceRatIds[i]);
-            spaceRatIdToIndex[spaceRatIds[i]] = ratsStaked[msg.sender].length - 1;
+            ratsStakedIds[msg.sender].push(spaceRatIds[i]);
+            spaceRatIdToIndex[spaceRatIds[i]] = ratsStakedIds[msg.sender].length - 1;
         }
         totalRatsStaked += amount;
         emit RatsStaked(msg.sender, spaceRatIds);
@@ -70,7 +70,7 @@ contract AsteroidMine is ReentrancyGuard {
             stakedAssets[spaceRatIds[i]] = address(0);
 
             // List of Rat IDs staked before withdrawal.
-            uint256[] storage userTokens = ratsStaked[msg.sender];
+            uint256[] storage userTokens = ratsStakedIds[msg.sender];
             // Index of the Rat being withdrawn.
             uint256 index = spaceRatIdToIndex[spaceRatIds[i]];
             // Index of the last token in the list of staked rats.
@@ -101,7 +101,7 @@ contract AsteroidMine is ReentrancyGuard {
     }
 
     function withdrawAll() external {
-        withdrawSpaceRats(ratsStaked[msg.sender]);
+        withdrawSpaceRats(ratsStakedIds[msg.sender]);
         claimRewards();
     }
 
@@ -119,17 +119,17 @@ contract AsteroidMine is ReentrancyGuard {
     //      The LastUpdatedTimestamp will be updated and the process continues.
     // TODO: Add tests for this.
     modifier updateReward(address user) {
-        uint256 userRatsStaked = ratsStaked[user].length;
+        uint256 userRatsStaked = ratsStakedIds[user].length;
         uint256 currentReward = userRewards[user];
         uint256 lastReward = userRewardsLastUpdate[user];
 
-        uint256 newReward = userRatsStaked * getSecondsSinceLastUpdated(user);
+        uint256 newReward = userRatsStaked * getSecondsSinceLastRewardUpdate(user) * rewardRate;
         userRewards[user] = currentReward + newReward;
         userRewardsLastUpdate[user] = block.timestamp;
         _;
     }
 
-    function getSecondsSinceLastUpdated(address user) public view returns (uint256) {
+    function getSecondsSinceLastRewardUpdate(address user) public view returns (uint256) {
         uint256 prevTimestamp = userRewardsLastUpdate[user];
         if (prevTimestamp > 0) {
             return block.timestamp - prevTimestamp;
